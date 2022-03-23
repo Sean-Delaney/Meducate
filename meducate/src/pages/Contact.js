9da9c13d2e0288c627ebf946c4e06f0794ef1165
Sean Delaney <seanrdelaney17@gmail.com>
2022-03-23 14:25:29 +0000

    Created Contact placeholder page, added comments

---
+++ meducate/src/pages/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../components/css/contact.css'
+
+//Contact page
+const Contact = () => {
+  //HTML to be rendered
+  return (
+    <div id="contact">Eu aliquip magna anim magna exercitation consectetur anim eu consectetur laboris sint mollit. Quis nisi enim magna nisi nulla aliquip est sunt nulla anim nostrud. Eu anim eiusmod nulla veniam. Excepteur officia commodo ad exercitation sunt do ex minim amet. Eu ex reprehenderit laboris cillum consectetur nostrud excepteur laboris mollit et in sint. Non non magna id magna duis quis tempor sunt.
+
+Ad sint aliquip duis nulla duis nisi culpa voluptate duis ipsum. Reprehenderit cillum est mollit adipisicing exercitation laborum dolore dolor pariatur in amet quis ut. Sunt culpa occaecat irure sint. Fugiat consequat quis Lorem ipsum minim eu labore proident.
+
+Esse mollit anim velit eiusmod amet ex exercitation ipsum consequat. Sunt do ad laborum deserunt enim adipisicing dolore aliqua ad minim amet ex Lorem. Voluptate pariatur aliqua excepteur sit amet voluptate proident nostrud reprehenderit. Nulla deserunt consectetur ex ut. Aliqua sit ipsum eiusmod consectetur aliqua nulla excepteur elit cupidatat officia culpa deserunt. In cillum ipsum nulla exercitation aliqua sunt sint eiusmod adipisicing.
+
+Eiusmod nostrud ad ipsum laborum. Commodo in in exercitation mollit reprehenderit non ullamco quis in sit nisi labore reprehenderit. Quis mollit nisi nisi commodo dolore. Officia consequat occaecat ullamco deserunt laborum laboris culpa esse. Irure voluptate aliquip labore laboris est dolor laboris duis. Eu ut cupidatat adipisicing adipisicing enim ullamco officia.
+
+Aliquip adipisicing et et ex do adipisicing aliquip dolore deserunt labore commodo eu. Esse enim ea velit esse in laborum fugiat magna. Irure occaecat quis occaecat anim deserunt incididunt proident tempor culpa. Aliquip labore occaecat dolore do sunt cupidatat incididunt aliqua voluptate dolor aute culpa magna. Ea officia et do exercitation aliquip adipisicing commodo duis non qui excepteur.
+
+Tempor deserunt et adipisicing labore nulla officia. Excepteur incididunt occaecat consequat nisi sit aliquip ex voluptate. Velit in quis nostrud est ullamco irure commodo ullamco fugiat nostrud adipisicing. Enim irure do nulla ad culpa labore duis veniam velit ut do esse quis magna.
+
+Enim nisi laboris minim pariatur cupidatat ullamco mollit in est minim officia eu veniam do. Mollit Lorem labore aute proident. Pariatur aute consectetur occaecat non consequat eiusmod irure culpa quis. Aliqua culpa minim voluptate officia anim exercitation non eiusmod excepteur excepteur.
+
+Magna elit id esse consequat sit culpa nulla proident. Cupidatat reprehenderit duis officia enim Lorem elit dolor Lorem velit nisi voluptate. Labore fugiat eiusmod et sint. Consectetur qui incididunt adipisicing magna et ad nostrud. Labore culpa in ut occaecat aute nulla cupidatat in nostrud. Ex sunt nulla in excepteur mollit incididunt aute aliqua sit ex amet culpa aute.
+
+Dolor voluptate pariatur veniam occaecat voluptate irure. Ex sit aliqua cupidatat do adipisicing dolor. Officia minim ut aliquip eu in mollit anim nostrud.
+
+Aute non culpa Lorem anim ad et eiusmod irure enim et sunt esse. Culpa dolor irure id incididunt velit enim aute laboris tempor eiusmod culpa mollit dolore non. Consequat dolor nisi fugiat nulla fugiat consequat et adipisicing. In dolore consectetur veniam quis.</div>
+  )
+}
+
+export default Contact
