67f62595644c551e31f46e3b9f100377c63e6a84
Sean Delaney <seanrdelaney17@gmail.com>
2022-04-05 18:47:19 +0100

    updtated post requests inuser API

--- Server/routes/Users.js
+++ Server/routes/Users.js
@@ -17,14 +17,36 @@ router.get('/', (req, res) => {
 
 //Route for POST requests
 router.post('/', async (req, res) => {
-  var user = req.body;
-  await usermodel.insertMany((user), (err) => {
+  //Get the username
+  query = {
+    username : req.body.username
+  }
+  //Check if the uer already exists
+  usermodel.find(query, async (err, data)=>{
+    //Handle any errors
     if(err){
       console.log(err);
     }
-  });
-  res.send('success');
-  res.end();
+    //If the details already exist
+    if(data.length > 0){
+      res.send('User already exists')
+      res.end();
+    }
+    //If not, enter the user
+    else{
+      var user = req.body;
+      await usermodel.insertMany((user), (err) => {
+        if(err){
+        console.log(err);
+        }else{
+          //Show the console the new user has been inserted.
+          console.log('user ' + req.body.username + ' inserted')
+        }
+      });
+      res.send('success');
+      res.end();
+    }
+  })
 })
 
 module.exports = router;
