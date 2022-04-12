0de9276aa8d3a848c3d8e9f405d29e38805f42b2
Sean Delaney <seanrdelaney17@gmail.com>
2022-04-13 00:20:11 +0100

    Small tweaks to DNS names

--- Client/meducate/src/components/Druglist.js
+++ Client/meducate/src/components/Druglist.js
@@ -6,7 +6,7 @@ const Druglist = ( {name, url} ) => {
   const[substances, setSubstances] = useState([])
 
   useEffect(()=>{
-    axios.get('http://ec2-3-87-93-124.compute-1.amazonaws.com/drugs', 
+    axios.get('http://ec2-3-87-93-124.compute-1.amazonaws.com/drugs',
     {params: {
       name: name
     }}).then((res)=>{
